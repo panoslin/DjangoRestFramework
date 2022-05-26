1c988f15564e28238bce1a60109b2e1665be3f1c
Panos <panoslin@foxmail.com>
2022-05-26 19:43:46 +0800

    add: article viewset

--- blog/article/views.py
+++ blog/article/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from rest_framework import (
+    filters,
+    viewsets,
+)
+from .models import Article
+from .serializers import (PaginationSerializer, ArticleListSerializer)
+from .filters import ArticleFilter
+from rest_framework_filters.backends import RestFrameworkFilterBackend
 
-# Create your views here.
+
+class ArticleViewSet(viewsets.ModelViewSet):
+    model = Article
+    serializer_class = ArticleListSerializer
+    filter_backends = (
+        RestFrameworkFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter
+    )
+    filterset_class = ArticleFilter
+    pagination_class = PaginationSerializer
+    search_fields = [
+        "title",
+        "desc",
+        "content",
+        "author"
+    ]
+    ordering_fields = [
+        "created_at",
+        "title"
+    ]
+    ordering = [
+        "created_at"
+    ]
+    queryset = Article.objects.all()
+
+    # def create(self, request, *args, **kwargs):
+    #     pass
+
+    # def update(self, request, *args, **kwargs):
+    #     pass
+
+    # def partial_update(self, request, *args, **kwargs):
+    #     pass
+
+    # def destroy(self, request, *args, **kwargs):
+    #     pass
