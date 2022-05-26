ac4384bf4c37d0dbfbd840729ff5474711d3893f
Panos <panoslin@foxmail.com>
2022-05-26 19:42:06 +0800

    add: article admin

--- blog/article/admin.py
+++ blog/article/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Article
 
-# Register your models here.
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    fields = (
+        "title",
+        "desc",
+        "content",
+
+        "status",
+
+        "author",
+    )
+
+    list_display = ("id", "title", "status", "author")
+    list_display_links = ("id", "title")
+
+    list_filter = ("status",)
+
+    search_fields = ("title", "desc", "content", "author")
